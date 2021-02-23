4547d2a9689827f86b22e791ad3330acabd1151c
Duarte Silveira <duartesilveira@Duartes-MacBook-Air.local>
2021-02-23 16:23:29 -0300

    Fibonacci Function Done

--- fibonacci.py
+++ fibonacci.py
@@ -7,7 +7,7 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 #Set n numbers
-n = 4
+n = 35
 #Return a list of n values of a Fibonacci sequence
 Fibonacci_sequence = [fibonacci(i) for i in range(n)]
 #Print the results
