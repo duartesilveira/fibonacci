9c1b7da8a6391e5031d89768c46d1e231f32f496
Duarte Silveira <duartesilveira@Duartes-MacBook-Air.local>
2021-02-23 16:10:00 -0300

    Created repo

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+
+def fibonacci(n):
+    "Simple function that returns N numbers of a Fibonnaci sequence"
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+#Set n numbers
+n = 4
+#Return a list of n values of a Fibonacci sequence
+Fibonacci_sequence = [fibonacci(i) for i in range(n)]
+#Print the results
+print(Fibonacci_sequence)
