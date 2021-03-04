e1d6dad05194d42bb4c32406db1ace91477c7368
Enrique Ferrer Agius <kike20ferrer@gmail.com>
2021-03-04 18:14:10 +0100

    first commit kike

--- fibonacci.py
+++ fibonacci.py
@@ -21,10 +21,15 @@ def fibonacci(element):
 n = 35
 limit = 0
 
+a = 3
+
 # Return a list of n values of a Fibonacci sequence
 Fibonacci_sequence = [fibonacci(i) for i in range(n)]
 Fibbonacci_limit = fibonacci(limit)
+Fibonacci_kike = fibonacci(a)
 
 # Print the results
 print(Fibonacci_sequence)
 print(Fibbonacci_limit)
+print(Fibonacci_kike)
+
